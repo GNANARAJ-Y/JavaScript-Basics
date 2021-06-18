2a13f7b98fc60d38b162ee3522f27054e0acb8d8
GNANARAJ-Y <85892018+GNANARAJ-Y@users.noreply.github.com>
2021-06-18 09:58:36 +0530

    Add files via upload
    Task given JSON using for..of statement - 17.06.2021

---
+++ json forof.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+var obj = [{ person: "Name1", age: "2", company: "GUVI" },
+          { person: "Name2", age: "5", company: "GUVI geek" },
+          { person: "Name3", age: "8", company: "GUVI geek network" }];
+
+ for(var i of obj)
+    {
+        console.log("Person : " + i.person);
+        console.log("Age : " + i.age);
+        console.log("Company : " + i.company);
+        
+    }
+});
+
